3513761544cc0463b20cf91390da1b59288a8fa9
laravel <abbas.ajorlou1371@gmail.com>
2023-01-11 12:39:16 +0330

    User offline status check schedule added

--- app/Console/Commands/CalculateFeatureProfit.php
+++ app/Console/Commands/CalculateFeatureProfit.php
@@ -2,9 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Events\UserStatusChanged;
 use App\Models\Feature;
 use Illuminate\Console\Command;
 use App\Models\Feature\FeatureHourlyProfit;
+use App\Models\User;
 
 class CalculateFeatureProfit extends Command
 {
@@ -29,15 +31,24 @@ class CalculateFeatureProfit extends Command
      */
     public function handle()
     {
-        foreach (FeatureHourlyProfit::lazy() as $profit)
-        {
-            if (now() < $profit->dead_line)
-            {
-                if($profit->updated_at->diffInHours(now()) >= 3) {
+        foreach (FeatureHourlyProfit::lazy() as $profit) {
+            if (now() < $profit->dead_line) {
+                if ($profit->updated_at->diffInHours(now()) >= 3) {
                     $feature = Feature::with('properties')->where('id', $profit->feature_id)->first();
                     $profit->increment('amount', $feature->properties->stability * 0.000041666);
                 }
             }
         }
+
+        User::where('last_seen', '<', now()->subMinutes(2))
+        ->select(['id', 'code'])
+        ->chunkById(100, function ($users) {
+            foreach ($users as $user) {
+                broadcast(new UserStatusChanged([
+                    'code' => $user->code,
+                    'status' => 'offline'
+                ]));
+            }
+        });
     }
 }
