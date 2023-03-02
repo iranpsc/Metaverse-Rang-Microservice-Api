779d9179c06218bc6d8bed6c175177a0209d9ad3
abbas <abbas.ajorlou1371@gmail.com>
2023-03-02 16:00:01 +0330

    reset  password pattern improved

--- app/Http/Controllers/Api/V1/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Api/V1/Auth/ResetPasswordController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Validation\Rules\Password;
 
 class ResetPasswordController extends Controller
 {
     use ResetsPasswords;
+    protected function rules()
+    {
+        return [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => ['required', 'confirmed', Password::min(8)->mixedCase()->numbers()->symbols()],
+        ];
+    }
 }
