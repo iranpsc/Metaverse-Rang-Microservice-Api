5a677c2c6424499a84125240a3e4148d85eb1050
abbas <abbas.ajorlou1371@gmail.com>
2023-03-17 13:16:22 +0330

    Notifications count added to ProfileResource

--- app/Http/Resources/ProfileResource.php
+++ app/Http/Resources/ProfileResource.php
@@ -24,6 +24,7 @@ class ProfileResource extends JsonResource
             'level' => $this->level,
             'score_percentage_to_next_level' => $this->level?->getScorePercentageToNextLevel($this->resource) ?? 0,
             'hourly_profit_time_percentage' => hourlyProfitInfo($this->resource),
+            'notifications' => $this->unreadNotifications->count(),
             'wallet' => new AssetResource($this->assets),
             'image' => $this->profilePhotos->last()?->url,
             $this->mergeWhen($this->features->count() > 0, [
