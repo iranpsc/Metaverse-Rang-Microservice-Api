607333ab6a629302bc30d1c89ac1ce6b70d46632
abbas <abbas.ajorlou1371@gmail.com>
2023-05-02 09:37:34 +0330

    Videos small bug fix

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
@@ -18,20 +17,6 @@ class Video extends Model
         $this->views()->updateOrCreate(
             ['ip_address' => request()->ip()],
             ['ip_address' => request()->ip()]
-        );
-    }
-
-    protected function fileName(): Attribute
-    {
-        return Attribute::make(
-            get: fn ($value) => config('rgb.admin_panel_url') . $value
-        );
-    }
-
-    protected function image(): Attribute
-    {
-        return Attribute::make(
-            get: fn ($value) => config('rgb.admin_panel_url') . $value
         );
     }
 
