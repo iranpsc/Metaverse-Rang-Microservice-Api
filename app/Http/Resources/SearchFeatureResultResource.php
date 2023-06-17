74ee2fb4288e61a9de028608e2f2f3507f156efc
Abbas Ajorloo <abbas.ajorlou1371@gmail.com>
2023-06-17 19:18:51 +0330

    Search features small bug fix

--- app/Http/Resources/SearchFeatureResultResource.php
+++ app/Http/Resources/SearchFeatureResultResource.php
@@ -21,7 +21,7 @@ class SearchFeatureResultResource extends JsonResource
             'id' => $this->feature->id,
             'feature_properties_id' => Str::upper($this->id),
             'address' => $this->address,
-            'karbari' => FeatureHelper::getFeatureName($this->feature),
+            'karbari' => $this->feature->getApplicationTitle(),
             'price_psc' => $this->price_psc,
             'price_irr' => $this->price_irr,
             'owner_code' => Str::upper($this->feature->owner->code),
