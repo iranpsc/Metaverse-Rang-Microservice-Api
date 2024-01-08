ddb4a3485eac8055dab654d784b78dd9b0f7b5c0
Abbas <abbas.ajorlou1371@gmail.com>
2024-01-08 16:19:03 +0330

    Remove unnecessary code in DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('feature_properties')->where('karbari', 't')->where('rgb', 'n')->
-        update(['rgb' => 'k']);
+        // 
     }
 }
