f24b813db0d9cc08cfe3701e4d92b4a309323c6e
abbass <abbas.ajorlou1371@gmail.com>
2024-11-15 14:54:42 +0330

    Add getFullUrlAttribute method to Image model for full image URL retrieval

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -23,4 +23,14 @@ class Image extends Model
     {
         return $this->morphTo();
     }
+
+    /**
+     * Get the full url of the image.
+     *
+     * @return string
+     */
+    public function getFullUrlAttribute()
+    {
+        return asset('uploads/' . $this->url);
+    }
 }
